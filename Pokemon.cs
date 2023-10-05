dc5fd4d96f49754341b0369b2dfe8a40b447be1d
Diego Silva <diego.silperes@gmail.com>
2023-10-04 20:02:43 -0400

    criando classe Pokemon

---
+++ Pokemon.cs
@@ -0,0 +1,16 @@
+﻿using DesafioPokemon;
+
+public class Pokemon
+{
+    public IEnumerable<Ability>? results { get; set; }
+
+    public void Exibir()
+    {
+        Console.WriteLine("Pokemons");
+        foreach (var item in results)
+        {
+            Console.WriteLine($"Nome: {item.name}");
+            Console.WriteLine($"Url: {item.url}");
+        }
+    }
+}
