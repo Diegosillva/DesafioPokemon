067cb3e332a5d122dcbc7193c5dd1574e86759aa
Diego Silva <diego.silperes@gmail.com>
2023-10-04 20:04:34 -0400

    comunicando com Api

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DesafioPokemon;
+using Newtonsoft.Json;
+using System.Text.Json;
+
+using (var client  = new HttpClient())
+{
+    var url = "https://pokeapi.co/api/v2/pokemon";
+    var response = await client.GetStringAsync(url);
+    Pokemon pokemon = JsonConvert.DeserializeObject<Pokemon>(response)!;
+    pokemon.Exibir();
+}
